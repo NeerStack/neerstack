0c2ecb74b4f445ca71ebd080a6883e6feddd957e
Raqeeb Taiwo <taiworoqeeb@gmail.com>
2022-10-24 17:26:37 +0100

    -a

--- pages/software-test.vue
+++ pages/software-test.vue
@@ -5,12 +5,36 @@
            Software Testing
         </h1>
     </div> 
+    <section>
+        <div>
+           <p>
+            Software testing is a process of examining the artifacts and the behavior of software under test, whether in a business or educational context. It can also provide an objective, independent view of the software to allow the business to appreciate and understand the risks of software implementation.
+           </p> 
+           <p>
+            Software testing is the process of examining a software program or application, as well as its associated test data, and determining if a particular test has passed. Software testing examines the artifacts and behavior of the software under test and ensures that it conforms to the intended specifications.
+           </p>
+           <p>
+            Software testing is an important activity in any software development process. As the name implies, software testers ensure that software meets defined and agreed-upon requirements. When they are done properly, they can help make sure that an application is easily used by potential users and provides satisfactory performance in the real world.
+           </p>
+        </div>
+    </section>
     </main>
     
 </template>
 <style scoped>
-main{
-    margin-top: 180px;
-    margin-bottom: 180px;
-}
+    main{
+        margin-top: 120px;
+        margin-bottom: 120px;
+    }
+    h1{
+        color: black;
+        text-transform: uppercase;
+    }
+    p{
+        font-size: medium;
+        text-align: justify;
+        margin: 10px 25px 10px 25px;
+        padding: 10px 25px 10px 25px;
+        color: black;
+    }
 </style>
